cf6b6f3292b39ee6163ee97949537ca282a1b9ab
Noor-devops <noorshanu07@gmail.com>
2026-06-17 09:26:20 +0530

    hello world script created

--- roboshop-v2.sh
+++ roboshop-v2.sh
@@ -34,8 +34,8 @@ get_instance_id(){
 for instance in $@
 do
     INSTANCE_ID=$(get_instance_id $instance)
-    if [ $ACTION eq "create" ]; then
-        if [ $INSTANCE_ID eq "None" ]; then
+    if [ $ACTION = "create" ]; then
+        if [ $INSTANCE_ID = "None" ]; then
             echo "Launching Instance: roboshop-$instance"
             INSTANCE_ID=$( aws ec2 run-instances \
             --image-id $AMI_ID \
